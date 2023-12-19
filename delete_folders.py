41af53a60cddd1db356d65dc660c256653c4238a
Craig <92700720+Craig-G-NZ@users.noreply.github.com>
2023-12-20 10:21:25 +1300

    Update delete_folders.py

--- delete_folders.py
+++ delete_folders.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import requests
 from datetime import datetime
 
 # Function to delete empty folders and log deleted folder names
@@ -14,16 +15,27 @@ def delete_empty_folders(path):
                 os.rmdir(folder_path)
     return deleted_folders
 
+# Function to send a message to Telegram
+def send_to_telegram(api_token, chat_id, message):
+    api_url = f'https://api.telegram.org/bot{api_token}/sendMessage'
+    try:
+        response = requests.post(api_url, json={'chat_id': chat_id, 'text': message})
+        print(response.text)
+    except Exception as e:
+        print(e)
+
 # Main script
 print("Searching for empty folders...")
 
-# Check if the search path is provided as a command line argument
-if len(sys.argv) != 2:
-    print("Error: Please provide the search path as a command line argument.")
+# Check if the required command line arguments are provided
+if len(sys.argv) != 4:
+    print("Error: Please provide the search path, API token, and chat ID as command line arguments.")
     sys.exit(1)
 
-# Get the search path from the command line argument
+# Get command line arguments
 search_path = sys.argv[1]
+api_token = sys.argv[2]
+chat_id = sys.argv[3]
 
 # Check if the provided path exists
 if os.path.isdir(search_path):
@@ -37,6 +49,15 @@ if os.path.isdir(search_path):
         with open(log_filename, 'w') as log_file:
             for folder in deleted_folders:
                 log_file.write(folder + '\n')
+
+        # Send a notification to Telegram
+        event_name = 'Deleted folders'
+        message = f"{event_name}:\n\n"
+        for folder in deleted_folders:
+            message += f"- {folder}\n"
+
+        send_to_telegram(api_token, chat_id, message)
+
         print(f"Empty folders deleted successfully. Deleted folders listed in {log_filename}.")
     else:
         print("No empty folders found.")
